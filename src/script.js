a85a5596135c3a4246bad3feff4e136e9661b6e0
TimD <timothee.diroian@gmail.com>
2022-05-25 09:02:55 +0200

    adding conditionnal in roll function for option argument and advt disadvt or undef

--- src/script.js
+++ src/script.js
@@ -4,8 +4,27 @@ function roll(sideCount, option) {
 
     // Defined a function who will stock the randome roll of dice, the +1 avoid the randome number to be 0
     const roll =()=> Math.floor(Math.random()*sideCount)+1;
+	
+	// Roll the dice twice *Hope we will have a critical sucsses*
+    const roll1 = roll();
+    //console.log(roll1);
+    const roll2 = roll();
+    //console.log(roll2);
 
-	return roll(sideCount)
+    // Check the state of the option argument
+    if(option == 'advantage'){
+        //console.log(Math.max(roll1, roll2))
+        return Math.max(roll1, roll2);
+    } else if(option == 'disadvantage'){
+        //console.log(Math.min(roll1, roll2))
+        return Math.min(roll1, roll2);
+    } else if(option == undefined){
+        //console.log(roll1)
+        return roll1;
+    } else{
+        // In case of mispelling I made the console Throw an error message explaining what's exepting in the option argument.
+        throw new Error("If you want to try the option type 'advantage' or 'disadvange' in the option arguments if you dont want them just let option blank.")
+    };
 }
 
 // Exportation for the testing
--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ describe('Random_num', function(){
     it('Should return an numbers who will be between 1 to a max value who will be defined in the first argument of the function.', function(){
         var minValue = 1
         var maxValue = 4;
+        // Test for undefined option argument
         var output = roll(maxValue);
         var controle = (minValue <= output && output <= maxValue);
         assert.strictEqual(controle, true, 'The value is not between 1 and 4')
