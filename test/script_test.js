4820be350ba1fa6f36bd59db828b697ce105987d
TimD <timothee.diroian@gmail.com>
2022-05-25 09:40:59 +0200

    Adding unit test for option and undefiened + create a script_test.js file to avoid conflic.

--- test/script_test.js
+++ test/script_test.js
@@ -1,5 +1,3 @@
-// Finish the program
-// If you want the option in your roll write the string 'advantage' or 'disadvantage' when calling the function.
 function roll(sideCount, option) {
 
     // Defined a function who will stock the randome roll of dice, the +1 avoid the randome number to be 0
@@ -27,15 +25,5 @@ function roll(sideCount, option) {
     };
 };
 
-// output test
-const log = (msg) => {
-	const divElement = document.createElement("span");
-	divElement.innerText = JSON.stringify(msg);
-	document.getElementById("log").append(divElement);
-}
-
-// Try uncommenting this
-Array(100).fill(null).forEach(() => log(roll(4)))
-// Or try one of the option
-// Array(100).fill(null).forEach(() => log(roll(4,'advantage')))
-// Array(100).fill(null).forEach(() => log(roll(4,'disadvantage')))
+// Exportation for the testing
+module.exports = roll;
--- test/test.js
+++ test/test.js
@@ -1,13 +1,25 @@
-var roll = require('../src/script')
+var roll = require('./script_test')
 var assert = require('assert');
 
 describe('Random_num', function(){
-    it('Should return an numbers who will be between 1 to a max value who will be defined in the first argument of the function.', function(){
-        var minValue = 1
-        var maxValue = 4;
-        // Test for undefined option argument
-        var output = roll(maxValue);
-        var controle = (minValue <= output && output <= maxValue);
-        assert.strictEqual(controle, true, 'The value is not between 1 and 4')
-    })
+    var minValue = 1
+    var maxValue = 4;
+    it('Should return an numbers between 1 to a max value who will be defined in the first argument with undefined option argument.', function(){
+        // Test for undefined option argument 100 time.
+        var output = new Array(100).fill(0).map(number => number + roll(maxValue));
+        var controle = output.map(number => minValue <= number <= maxValue)
+        assert.strictEqual(controle.every(number => number == true), true, 'Some values is not between 1 and 4')
+    });
+    it('Should return an numbers between 1 to a max value who will be defined in the first argument with the advantage option argument.', function(){
+        // Test for 'advantage' option argument 100 time.
+        var outputAdv = new Array(100).fill(0).map(number => number + roll(maxValue, 'advantage'));
+        var controleAdv = outputAdv.map(number => minValue <= number <= maxValue)
+        assert.strictEqual(controleAdv.every(number => number == true), true, "Some values is not between 1 and 4 with 'advantage' option argument")
+    });
+    it('Should return an numbers between 1 to a max value who will be defined in the first argument with the disadvantage option argument.', function(){
+        // Test for 'disadvantage' option argument 100 time.
+        var outputDisadv = new Array(100).fill(0).map(number => number + roll(maxValue, 'disadvantage'));
+        var controleDisav = outputDisadv.map(number => minValue <= number <= maxValue)
+        assert.strictEqual(controleDisav.every(number => number == true), true, "Some values is not between 1 and 4 with 'disadvantage' option argument")
+    });
 });
