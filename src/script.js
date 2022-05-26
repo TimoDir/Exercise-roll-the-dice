d6d18b1a4af3d3f5587464fcf767f744b6235b71
TimD <timothee.diroian@gmail.com>
2022-05-26 13:25:04 +0200

    correction clear comments

--- src/script.js
+++ src/script.js
@@ -2,28 +2,25 @@
 // If you want the option in your roll write the string 'advantage' or 'disadvantage' when calling the function.
 function roll(sideCount, option) {
 
-    // Defined a function who will stock the randome roll of dice, the +1 avoid the randome number to be 0
+    // Defined a function who will stock the random roll of dice, the +1 avoid the random number to be 0
     const roll =()=> Math.floor(Math.random()*sideCount)+1;
 	
-	// Roll the dice twice *Hope we will have a critical sucsses*
+	// Roll the dice twice *Hope we will have a critical success*
     const roll1 = roll();
-    //console.log(roll1);
     const roll2 = roll();
-    //console.log(roll2);
 
     // Check the state of the option argument
     if(option == 'advantage'){
-        //console.log(Math.max(roll1, roll2))
+        // Return the highest value
         return Math.max(roll1, roll2);
     } else if(option == 'disadvantage'){
-        //console.log(Math.min(roll1, roll2))
+        // Return the lowest value
         return Math.min(roll1, roll2);
     } else if(option == undefined){
-        //console.log(roll1)
         return roll1;
     } else{
-        // In case of mispelling I made the console Throw an error message explaining what's exepting in the option argument.
-        throw new Error("If you want to try the option type 'advantage' or 'disadvange' in the option arguments if you dont want them just let option blank.")
+        // In case of misspelling I made the console Throw an error message explaining what's expecting in the option argument.
+        throw new Error("If you want to try the option type 'advantage' or 'disadvantage' in the option arguments if you don't want them just let option blank.")
     };
 };
 
--- test/test.js
+++ test/test.js
@@ -1,25 +1,26 @@
-var roll = require('./script_test')
+var roll = require('./script_test.mjs')
 var assert = require('assert');
 
 describe('Random_num', function(){
-    var minValue = 1
-    var maxValue = 4;
+    const minValue = 1
+    const maxValue = 4;
+    const expectedResult = new Array(100).fill(true);
     it('Should return an numbers between 1 to a max value who will be defined in the first argument with undefined option argument.', function(){
         // Test for undefined option argument 100 time.
-        var output = new Array(100).fill(0).map(number => number + roll(maxValue));
-        var controle = output.map(number => minValue <= number <= maxValue)
-        assert.strictEqual(controle.every(number => number == true), true, 'Some values is not between 1 and 4')
+        const output = new Array(100).fill(0).map(number => number + roll(maxValue));
+        const controle = output.map(number => minValue <= number <= maxValue);
+        assert.strictEqual(controle.includes(false), expectedResult.includes(false), 'Some values is not between 1 and 4')
     });
     it('Should return an numbers between 1 to a max value who will be defined in the first argument with the advantage option argument.', function(){
         // Test for 'advantage' option argument 100 time.
-        var outputAdv = new Array(100).fill(0).map(number => number + roll(maxValue, 'advantage'));
-        var controleAdv = outputAdv.map(number => minValue <= number <= maxValue)
-        assert.strictEqual(controleAdv.every(number => number == true), true, "Some values is not between 1 and 4 with 'advantage' option argument")
+        const output = new Array(100).fill(0).map(number => number + roll(maxValue, 'advantage'));
+        const controle = output.map(number => minValue <= number <= maxValue)
+        assert.strictEqual(controle.includes(false), expectedResult.includes(false), "Some values is not between 1 and 4 with 'advantage' option argument")
     });
     it('Should return an numbers between 1 to a max value who will be defined in the first argument with the disadvantage option argument.', function(){
         // Test for 'disadvantage' option argument 100 time.
-        var outputDisadv = new Array(100).fill(0).map(number => number + roll(maxValue, 'disadvantage'));
-        var controleDisav = outputDisadv.map(number => minValue <= number <= maxValue)
-        assert.strictEqual(controleDisav.every(number => number == true), true, "Some values is not between 1 and 4 with 'disadvantage' option argument")
+        const output = new Array(100).fill(0).map(number => number + roll(maxValue, 'disadvantage'));
+        const controle = output.map(number => minValue <= number <= maxValue)
+        assert.strictEqual(controle.includes(false), expectedResult.includes(false), "Some values is not between 1 and 4 with 'disadvantage' option argument")
     });
 });
